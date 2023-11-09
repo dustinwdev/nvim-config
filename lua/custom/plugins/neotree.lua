2369582dd7c82ac509adc4bb506bc92027dc3159
Dustin Wilson <36707563+dustinwdev@users.noreply.github.com>
2023-11-09 01:27:39 -0500

    Set up keymap for Neo-tree

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -6,5 +6,8 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    vim.keymap.set({ 'n', 'i' }, '<leader>t', function() vim.cmd('Neotree toggle') end, { desc = 'Neotree [T]oggle' })
+  end,
 }
